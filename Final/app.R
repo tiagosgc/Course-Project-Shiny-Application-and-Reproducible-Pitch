b716ae0394becc944a5e6f80fa1029a3ad1d545d
Tiago Carvalho <tiagosgc@gmail.com>
2017-02-07 22:52:36 +0000

    restart bug fixed

--- Final/app.R
+++ Final/app.R
@@ -23,13 +23,18 @@ ui <- fluidPage(
                      min = 1,
                      max = 100,
                      value = NULL),
-         submitButton("Guess!")
+         
+         radioButtons("radio", "Select Action", c("Play","Restart"), selected = "Play", inline = FALSE,
+                      width = NULL),
+         submitButton("Guess! ", icon=icon("share"))
+         #actionButton("reset","Reset the App")
          #"guessBtn", 
         ), 
       
       mainPanel(
          h1("Instructions on how to play:"),
          tags$ol(
+             tags$li("Choose the reset radio button and submit to start playing"), 
              tags$li("Just slide the slider to your guess."), 
              tags$li("Learn if you are higher or lower than the random number generated"),
              tags$li("Try to get there in 7 or less tries. Should be doable. Good luck.")
@@ -46,72 +51,99 @@ ui <- fluidPage(
          p(textOutput("text2")),
          p(textOutput("text1"))
          
+         
+         
       )
    )
 )
 
-debug <- function(guessArg) {
-    c(guessArg,"remaining:",remainingTriesStatic, " target:",target)
+debug <- function(guessArg,radioArg) {
+    c(guessArg,"remaining:",remainingTriesStatic, " target:",target, "radio:", radioArg)
 }
-displayLog<- function(guessArg,level)
+displayLog<- function(guessArg,level,radioArg)
 {
+    radioArg
     guessArg
-    log[level]
+    logg[level]
 }
 
-updateGuess <- function(guessArg) {
+updateGuess <- function(guessArg, radioArg) {
     #values$remainingTries <-values$remainingTries+ 1 
     #if (remainingTriesStatic==8) {
     #    remainingTriesStatic <<- remainingTriesStatic -1
     #    return
     #}
-    if (guessed == 1)
-    {
-        "You already guessed, please stop poking the slider"
+    guessArg
+    if (first==1) {
+        first<<-0
+        c(remainingTriesStatic," tries remaining")
     } else {
-        if (remainingTriesStatic==0) {
-            "Game Over - Please stop poking the slider"
-        }
-        else 
-        {
-            if (guessArg < target) {
-                newString<- paste(c(remainingTriesStatic,": ",guessArg, " is too LOW"),collapse='')
-            }
-            if (guessArg > target) {
-                newString<- paste(c(remainingTriesStatic,": ",guessArg, " is too HIGH"),collapse='')
-            }
-            if (guessArg == target) {
-                newString<- paste(c("Congratulations, you just guessed the right number: ",target),collapse = '')
-                guessed <<- 1 
-            }
-            log[remainingTriesStatic] <<- newString
-            remainingTriesStatic <<- remainingTriesStatic -1
-            #values$remainingTries <- values$remainingTries#values$remainingTries#- 1 
-            c(remainingTriesStatic," tries remaining")
-        }
+    if (radioArg == "Restart") {
+        init(7,0)
+        #c(remainingTriesStatic," tries remaining")
+        c(remainingTriesStatic," tries remaining")
     }
+    else {
+        if (guessed == 1)
+        {
+            "You already guessed, please stop poking the slider"
+        } else {
+            if (remainingTriesStatic==0) {
+                "Game Over - Please stop poking the slider"
+            }
+            else 
+            {
+                if (guessArg < target) {
+                    newString<- paste(c(remainingTriesStatic,": ",guessArg, " is too LOW"),collapse='')
+                }
+                if (guessArg > target) {
+                    newString<- paste(c(remainingTriesStatic,": ",guessArg, " is too HIGH"),collapse='')
+                }
+                if (guessArg == target) {
+                    newString<- paste(c("Congratulations, you just guessed the right number: ",target),collapse = '')
+                    guessed <<- 1 
+                }
+                logg[remainingTriesStatic] <<- newString
+                remainingTriesStatic <<- remainingTriesStatic -1
+                #values$remainingTries <- values$remainingTries#values$remainingTries#- 1 
+                c(remainingTriesStatic," tries remaining")
+            }
+        }
+    }}
     
 }
 
 server <- function(input, output) {
-    
-    output$title <- renderText({updateGuess(input$guess)})
-    output$debug <- renderText({debug(input$guess)})
-    output$text7 <-  renderText({displayLog(input$guess,7)})
-    output$text6 <-  renderText({displayLog(input$guess,6)})
-    output$text5 <-  renderText({displayLog(input$guess,5)})
-    output$text4 <-  renderText({displayLog(input$guess,4)})
-    output$text3 <-  renderText({displayLog(input$guess,3)})
-    output$text2 <-  renderText({displayLog(input$guess,2)})
-    output$text1 <-  renderText({displayLog(input$guess,1)})
+    #observeEvent(input$reset, {
+    #    output$title <- renderText({updateGuess(input$guess)})
+    #    remainingTriesStatic<-7 
+    #})
+    #output$restart <- eventReactive(input$reset,
+    #              {
+    #                remainingTriesStatic<-7
+    #                '.'
+    #              })
+    output$title <- renderText({updateGuess(input$guess, input$radio)})
+    #output$debug <- renderText({debug(input$guess, input$radio)})
+    output$text7 <-  renderText({displayLog(input$guess,7, input$radio)})
+    output$text6 <-  renderText({displayLog(input$guess,6, input$radio)})
+    output$text5 <-  renderText({displayLog(input$guess,5, input$radio)})
+    output$text4 <-  renderText({displayLog(input$guess,4, input$radio)})
+    output$text3 <-  renderText({displayLog(input$guess,3, input$radio)})
+    output$text2 <-  renderText({displayLog(input$guess,2, input$radio)})
+    output$text1 <-  renderText({displayLog(input$guess,1, input$radio)})
     
     
     
 }
-guessed=0
-remainingTriesStatic <- 8
-target=as.integer(runif(1,1,100))
-log=c(rep("",7))
+init <- function(remaining, first) {
+    guessed<<-0
+    remainingTriesStatic <<- remaining
+    target<<-as.integer(runif(1,1,100))
+    logg<<-c(rep("",7))
+    first<<-first
+}
+init(7,1)
 #labels[2] <- "label 2"
 #labels[3] <- "label 3"
 #labels[4] <- "label 4"
